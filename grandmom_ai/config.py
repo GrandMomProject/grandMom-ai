90aa1a2da3124e6ab03121765cca5efbf7f855b2
JeongMyeongHong <hkdhjm@gmail.com>
2024-07-27 03:26:05 +0900

    Remove config.py from remote repository

--- grandmom_ai/config.py
+++
@@ -1,4 +0,0 @@
-first_interview = {"host": "https://clovastudio.stream.ntruss.com",
-                   "api_key": "NTA0MjU2MWZlZTcxNDJiY0wwzFv7i+ZWYPVXX+yV/+nmGNmUryPI6SzRrCccWwTn",
-                   "api_key_primary_val": "pbrV9D3FxgcpVrCz9vxTS2jswywlcReofAcVyUVx",
-                   "request_id": "99932699-77f7-44d0-b715-c0d4d459d528"}
